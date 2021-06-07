bba570851a7fea9dd3d6f80f5771186d0585f004
Zachary Howard <Zachary.Howard@outlook.com>
2021-06-07 18:10:00 +0100

    Remove batch+shuffle logic from dataset

--- mrf_map.py
+++ mrf_map.py
@@ -6,17 +6,11 @@ class PixelwiseDataset(IterableDataset):
     def __init__(self,
                  data,
                  labels,
-                 batch_size: int = 0,
-                 shuffle: bool = False,
                  *args,
                  **kwargs):
         super().__init__()
         self.data = data
         self.labels = labels
-        self.batch_size = batch_size
-
-        self.indices = None
-        self._shuffle = shuffle
 
     @property
     def num_columns(self):
@@ -30,19 +24,10 @@ class PixelwiseDataset(IterableDataset):
     def num_pixels(self):
         return self.num_columns * self.num_rows
 
-    @property
-    def shuffle(self) -> bool:
-        return self._shuffle
-
-    @shuffle.setter
-    def shuffle(self, value: bool):
-        self.indices = np.arange(self.num_pixels) if value else None
-
     def __len__(self):
-        return (self.num_columns * self.num_rows) // self.batch_size
+        return self.num_pixels
 
     def __getitem__(self, index):
-        index = self.indices[index] if self.shuffle else index
         x = index // self.num_columns
         y = index % self.num_columns
         return self.data[x][y], self.labels[x][y]
