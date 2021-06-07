20431dd7edcefffe80ec24d504f83dcd64f280bf
Zachary Howard <Zachary.Howard@outlook.com>
2021-06-07 21:20:57 +0100

    Working COHEN!!!!

--- cohen.py
+++ cohen.py
@@ -1,3 +1,7 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -41,27 +45,58 @@ class TrainingAlgorithm:
         self.optimiser.step()
         return predicted
 
-
-
     def save_model(self, filename):
         torch.save(self.network.state_dict(), f"models/{filename}.pth")
 
+
+def get_all_data_files(folder: str = "Train", *args, **kwargs):
+    fingerprint_path = f"Data/MRF_maps/ExactFingerprintMaps/{folder}/"
+    parameter_path = f"Data/MRF_maps/ParameterMaps/{folder}/"
+    f1 = sorted([file.split('.')[0] for file in os.listdir(fingerprint_path) if not file.startswith(".")])
+    f2 = sorted([file.split('.')[0] for file in os.listdir(parameter_path) if not file.startswith(".")])
+    if f1 != f2:
+        raise RuntimeError("Differing data inside Test/Train folders!")
+
+    for file_name in f1:
+        yield folder, file_name
+
+def plot_model(network, testing_data_files):
+    folder, file_name = next(testing_data_files)
+    dataset = PixelwiseDataset.from_file_name(folder, file_name)
+    predicted = network.forward(torch.FloatTensor(dataset.data).to("cuda")).to("cpu").detach().numpy()
+    predicted_t1, predicted_t2 = np.transpose(predicted, axes=(2, 0, 1))
+    actual_t1, actual_t2 = np.transpose(dataset.labels, axes=(2, 0, 1))
+
+    plt.matshow(predicted_t1)
+    plt.matshow(actual_t1)
+    plt.matshow(np.abs(actual_t1 - predicted_t1))
+
+    plt.matshow(predicted_t2)
+    plt.matshow(actual_t2)
+    plt.matshow(np.abs(actual_t2 - predicted_t2))
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    dataset_loader = DataLoader(PixelwiseDataset.from_file_name("Train", "subj1_fisp_slc4_4"),
-                                batch_size=1000,
-                                shuffle=True,
-                                pin_memory=True,
-                                collate_fn=PixelwiseDataset.collate_fn)
     network = CohenMLP()
-    trainer = TrainingAlgorithm(network, 0.05)
-    dataset_iter = iter(dataset_loader)
-    for i in range(52000):
-        data, labels = next(dataset_iter)
-        predicted = trainer.update(data, labels).to("cpu")
-        print(torch.abs((predicted - labels).mean()))
-
-
-
-
-
-
+    trainer = TrainingAlgorithm(network, 0.001)
+    testing_data_files = get_all_data_files("Test")
+    for i, (folder, file_name) in enumerate(get_all_data_files("Train")):
+        if i % 5 == 0 and i != 0:
+            plot_model(network, testing_data_files)
+        print(folder, file_name)
+        dataset_loader = DataLoader(PixelwiseDataset.from_file_name(folder, file_name),
+                                    batch_size=1000,
+                                    shuffle=True,
+                                    pin_memory=True,
+                                    collate_fn=PixelwiseDataset.collate_fn)
+        dataset_iter = iter(dataset_loader)
+        try:
+            data, labels = next(dataset_iter)
+            while True:
+                predicted = trainer.update(data, labels).to("cpu")
+                print(torch.abs((predicted - labels).mean()))
+                data, labels = next(dataset_iter)
+        except StopIteration:
+            continue
