66801e48f82ce25352dd62f6c17b91f765dcf630
Zachary Howard <Zachary.Howard@outlook.com>
2021-07-08 16:05:06 +0100

    update rnn

--- networks.py
+++ networks.py
@@ -22,11 +22,10 @@ class OksuzLSTM(nn.Module):
     def __init__(self):
         super().__init__()
         self.rnn1 = nn.LSTM(input_size=1, hidden_size=1, batch_first=True)
-        self.sig = nn.Sigmoid()
         self.l1 = nn.Linear(1000, 2)
 
     def forward(self, x):
         x, (hn, cn) = self.rnn1(x)
-        x = self.sig(x.reshape(x.shape[0], x.shape[1]))
+        x = x.reshape(x.shape[0], x.shape[1])
         x = self.l1(x)
         return x
