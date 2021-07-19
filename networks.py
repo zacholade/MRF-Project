cf7d7ebf2aaa6456217f7497db98ce194f0b024a
Zachary Howard <Zachary.Howard@outlook.com>
2021-07-19 18:38:42 +0100

    test lstm

--- networks.py
+++ networks.py
@@ -24,27 +24,24 @@ class CohenMLP(nn.Module):
 class OksuzLSTM(nn.Module):
     def __init__(self, input_size: int, hidden_size: int, num_layers: int = 1, bidirectional: bool = False):
         super().__init__()
-        self.rnn = nn.LSTM(input_size=1, hidden_size=hidden_size, num_layers=num_layers,
+        self.rnn = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
                            batch_first=True, bidirectional=bidirectional)
-        self.fc1 = nn.Linear(in_features=hidden_size * 20, out_features=2)
+        self.fc1 = nn.Linear(in_features=500, out_features=2)
 
     def forward(self, x):
-        for i in range(100):
-            x_ = np.arange(1000)
-            print(x.shape)
-            y = x[i].detach().cpu().numpy()
-            import matplotlib.pyplot as plt
-            plt.scatter(x_, y, s=1)
-            plt.show()
-
         batch_size = x.shape[0]
-        x = x.view(batch_size * 20, 50, 1)
+        x = x.view(batch_size, -1, self.rnn.input_size)
+
         lstm_out, (hn, _) = self.rnn(x)
+
+        # lstm_out = lstm_out.view(batch_size, -1)
         hn = hn.view(batch_size, -1)
+
+        lstm_out = lstm_out.reshape(batch_size, -1)
+
         # lstm_out = lstm_out.view(batch_size, -1)
         # print(lstm_out.shape)
-        hn = hn.view(batch_size, -1)
-        fc_out = self.fc1(hn)
+        fc_out = self.fc1(lstm_out)
         return fc_out
 
     #
