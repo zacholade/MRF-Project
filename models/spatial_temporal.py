390b35bc23435ae622cb6629dd29863d69f9771c
Zachary Howard <Zachary.Howard@outlook.com>
2021-07-31 11:49:55 +0100

    add dropout

--- models/spatial_temporal.py
+++ models/spatial_temporal.py
@@ -13,27 +13,28 @@ class SpatioTemporal(nn.Module):
         super().__init__()
         self.conv1x1 = self.conv_block(in_channels=seq_len, out_channels=200,
                                        kernel_size=1, stride=1, padding='valid',
-                                       batch_norm=True, activation="relu")
+                                       dropout=True, batch_norm=True, activation="relu")
 
         self.conv3x3 = self.conv_block(in_channels=200, out_channels=32,
                                        kernel_size=3, stride=1, padding='valid',
-                                       batch_norm=True, activation="relu")
+                                       dropout=True, batch_norm=True, activation="relu")
 
         self.conv5x5 = self.conv_block(in_channels=200, out_channels=32,
                                        kernel_size=5, stride=1, padding='valid',
-                                       batch_norm=True, activation="relu")
+                                       dropout=True, batch_norm=True, activation="relu")
 
         self.conv_out = self.conv_block(in_channels=64, out_channels=2,
                                         kernel_size=1, stride=1, padding='valid',
-                                        batch_norm=False, activation=None)
+                                        dropout=False, batch_norm=False, activation=None)
 
         self.patch_size = patch_size
 
     def conv_block(self, in_channels, out_channels, kernel_size, stride: int, padding: str,
-                   batch_norm: bool, activation: Union[str, None] = 'none'):
+                   dropout: bool, batch_norm: bool, activation: Union[str, None] = 'none'):
         modules = [
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
                       kernel_size=kernel_size, stride=stride, padding=padding)]
+        dropout and modules.append(nn.Dropout2d(p=0.2))
         batch_norm and modules.append(nn.BatchNorm2d(out_channels))
         activation == 'relu' and modules.append(nn.ReLU())
         return nn.Sequential(*modules)
