52472f653745e1ab5a8736978c8b19fafd20799a
Zachary Howard <Zachary.Howard@outlook.com>
2021-08-01 23:10:56 +0100

    nograd

--- mrf_trainer.py
+++ mrf_trainer.py
@@ -163,44 +163,45 @@ class TrainingAlgorithm(LoggingMixin):
         chunk_predicted = None
         chunk_labels = None
         chunk_pos = None
-        for current_iteration, (data, labels, pos, file_name) in enumerate(validate_set):
-            current_chunk += 1
-            self.logger.info(f"Epoch: {epoch}, Validation scan: {(current_iteration // self.valid_chunks) + 1} / "
-                             f"{len(validate_loader) // self.valid_chunks}. "
-                             f"Chunk: {((current_chunk - 1) % self.valid_chunks) + 1} / "
-                             f"{self.valid_chunks}")
-            data, labels, pos = data.to(self.device), labels.to(self.device), pos.to(self.device)
-            predicted, loss, attention = self.spatial_validate(data, labels)
-            data, labels, pos = data.cpu(), labels.cpu(), pos.cpu()
-            chunk_loss += loss.detach().cpu().item()
-            chunk_predicted = predicted.detach().cpu() if chunk_predicted is None else \
-                torch.cat((chunk_predicted, predicted.detach().cpu()), 0)
-            chunk_labels = labels.detach().cpu() if chunk_labels is None else \
-                torch.cat((chunk_labels, labels.detach().cpu()), 0)
-            chunk_pos = pos.detach().cpu() if chunk_pos is None else \
-                torch.cat((chunk_pos, pos.detach().cpu()), 0)
-            if current_chunk % self.valid_chunks == 0:
-                self.data_logger.log_error(chunk_predicted,
-                                           chunk_labels,
-                                           chunk_loss, "valid")
+        with torch.no_grad():
+            for current_iteration, (data, labels, pos, file_name) in enumerate(validate_set):
+                current_chunk += 1
+                self.logger.info(f"Epoch: {epoch}, Validation scan: {(current_iteration // self.valid_chunks) + 1} / "
+                                 f"{len(validate_loader) // self.valid_chunks}. "
+                                 f"Chunk: {((current_chunk - 1) % self.valid_chunks) + 1} / "
+                                 f"{self.valid_chunks}")
+                data, labels, pos = data.to(self.device), labels.to(self.device), pos.to(self.device)
+                predicted, loss, attention = self.spatial_validate(data, labels)
+                data, labels, pos = data.cpu(), labels.cpu(), pos.cpu()
+                chunk_loss += loss.detach().cpu().item()
+                chunk_predicted = predicted.detach().cpu() if chunk_predicted is None else \
+                    torch.cat((chunk_predicted, predicted.detach().cpu()), 0)
+                chunk_labels = labels.detach().cpu() if chunk_labels is None else \
+                    torch.cat((chunk_labels, labels.detach().cpu()), 0)
+                chunk_pos = pos.detach().cpu() if chunk_pos is None else \
+                    torch.cat((chunk_pos, pos.detach().cpu()), 0)
+                if current_chunk % self.valid_chunks == 0:
+                    self.data_logger.log_error(chunk_predicted,
+                                               chunk_labels,
+                                               chunk_loss, "valid")
 
-                if self.plot_every > 0 and epoch % self.plot_every == 0:
-                    if not os.path.exists(f"{self.export_dir}/Plots"):
-                        os.mkdir(f"{self.export_dir}/Plots")
-                    # Matplotlib has a memory leak. To alleviate this do plotting in a subprocess and
-                    # join to it. When process is suspended, memory is forcibly released.
-                    plot(plot_maps,
-                         chunk_predicted.cpu().detach().numpy(),
-                         chunk_labels.cpu().numpy(),
-                         chunk_pos.cpu().numpy().astype(int),
-                         epoch,
-                         f"{self.export_dir}/Plots/{file_name}",
-                         file_name)
+                    if self.plot_every > 0 and epoch % self.plot_every == 0:
+                        if not os.path.exists(f"{self.export_dir}/Plots"):
+                            os.mkdir(f"{self.export_dir}/Plots")
+                        # Matplotlib has a memory leak. To alleviate this do plotting in a subprocess and
+                        # join to it. When process is suspended, memory is forcibly released.
+                        plot(plot_maps,
+                             chunk_predicted.cpu().detach().numpy(),
+                             chunk_labels.cpu().numpy(),
+                             chunk_pos.cpu().numpy().astype(int),
+                             epoch,
+                             f"{self.export_dir}/Plots/{file_name}",
+                             file_name)
 
-                chunk_loss = 0
-                chunk_predicted = None
-                chunk_labels = None
-                chunk_pos = None
+                    chunk_loss = 0
+                    chunk_predicted = None
+                    chunk_labels = None
+                    chunk_pos = None
 
     def _non_spatial_valid_loop(self, epoch, validate_loader):
         validate_set = iter(validate_loader)
@@ -267,13 +268,6 @@ class TrainingAlgorithm(LoggingMixin):
                     break  # If in debug mode and we dont want to run the full epoch. Break early.
 
                 current_iteration += 1
-                if current_iteration % max((total_iterations // 50), 1) == 0 or current_iteration == 2:
-                    self.logger.info(f"Epoch: {epoch}, Training iteration: {current_iteration} / "
-                                     f"{self.limit_iterations if (self.debug and self.limit_iterations > 0) else int(np.floor(len(training_dataset) / self.batch_size))}, "
-                                     f"LR: {self.lr_scheduler.get_last_lr()[0]}, "
-                                     f"Loss: {loss}")
-                    if attention is not None:
-                        plot(plot_fp, attention[0].detach().cpu().numpy(), f"{epoch}_{current_iteration}", save_dir=self.export_dir)
                 predicted, loss, attention = self.train(data, labels)
 
                 data, labels, pos = data.cpu(), labels.cpu(), pos.cpu()
@@ -281,6 +275,14 @@ class TrainingAlgorithm(LoggingMixin):
                                            labels.detach().cpu(),
                                            loss.detach().cpu().item(),
                                            data_type="train")
+
+                if current_iteration % max((total_iterations // 50), 1) == 0 or current_iteration == 2:
+                    self.logger.info(f"Epoch: {epoch}, Training iteration: {current_iteration} / "
+                                     f"{self.limit_iterations if (self.debug and self.limit_iterations > 0) else int(np.floor(len(training_dataset) / self.batch_size))}, "
+                                     f"LR: {self.lr_scheduler.get_last_lr()[0]}, "
+                                     f"Loss: {loss}")
+                    if attention is not None:
+                        plot(plot_fp, attention[0].detach().cpu().numpy(), f"{epoch}_{current_iteration}", save_dir=self.export_dir)
 
             if not skip_valid:
                 self.logger.info(f"Done training. Starting validation for epoch {epoch}.")
